ac79da4fa5e1464392ef4d5ad97421aa579d807b
Alex Partin <25892264+adpartin@users.noreply.github.com>
2023-12-06 11:12:49 -0800

    clean conda_env.sh

--- conda_env.sh
+++ conda_env.sh
@@ -2,18 +2,15 @@
 
 set -e
 
-# # Manually run these commands before running this sciprt
-# conda env create --file environment.yml --force
-
 # conda create -n lgbm_py37 python=3.7 pip --yes
 
-# Not required (my vim env)
-conda install -c conda-forge ipdb=0.13.9 --yes
-conda install -c conda-forge python-lsp-server=1.2.4 --yes
+# # Not required
+# conda install -c conda-forge ipdb=0.13.9 --yes
+# conda install -c conda-forge python-lsp-server=1.2.4 --yes
 
 # IMPROVE
 pip install git+https://github.com/ECP-CANDLE/candle_lib@develop # CANDLE
 
 # Other
-conda install -c conda-forge lightgbm --yes # LigthGBM
-pip install pyarrow # saves and loads parquet files
+conda install -c conda-forge lightgbm=3.1.1 --yes # LigthGBM
+pip install pyarrow=12.0.1 # saves and loads parquet files
