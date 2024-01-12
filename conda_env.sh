904f09d28018ce0dfbeca0826140bdf4004b280c
Sara Jones <91564739+jonesse3@users.noreply.github.com>
2024-01-12 13:20:49 -0500

    Update conda_env.sh
    Changed order of installing dependencies and included a line for activating the environment.

--- conda_env.sh
+++ conda_env.sh
@@ -9,8 +9,14 @@ set -e
 # conda install -c conda-forge python-lsp-server=1.2.4 --yes
 
 # IMPROVE
-pip install git+https://github.com/ECP-CANDLE/candle_lib@develop # CANDLE
+#pip install git+https://github.com/ECP-CANDLE/candle_lib@develop # CANDLE
 
 # Other
-conda install -c conda-forge lightgbm=3.1.1 --yes # LigthGBM
-pip install pyarrow=12.0.1 # saves and loads parquet files
+#conda install -c conda-forge lightgbm=3.1.1 --yes # LigthGBM
+#pip install pyarrow=12.0.1 # saves and loads parquet files
+
+conda create -n lgbm_py37 python=3.7 pip lightgbm=3.1.1 --yes
+conda activate lgbm_py37
+pip install pyarrow==12.0.1
+# IMPROVE
+pip install git+https://github.com/ECP-CANDLE/candle_lib@develop # CANDLE
