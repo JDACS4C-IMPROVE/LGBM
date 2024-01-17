cf2e8f57494eff785564f1318b17b70e78cdbdbe
Alex Partin <25892264+adpartin@users.noreply.github.com>
2024-01-17 12:25:08 -0800

    minor fix in infer_example script

--- infer_example.sh
+++ infer_example.sh
@@ -7,7 +7,7 @@
 #     --infer_outdir out_infer/GDSCv1-GDSCv1/split_0
 
 # Cross-study
-python frm_infer.py \
+python lgbm_infer_improve.py \
     --test_ml_data_dir ml_data/GDSCv1-CCLE/split_0 \
     --model_dir out_model/GDSCv1/split_0 \
     --infer_outdir out_infer/GDSCv1-CCLE/split_0
