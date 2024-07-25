32ac084b14f2151fc15d72f0f9e6edf6fe21b9e2
Alex Partin <25892264+adpartin@users.noreply.github.com>
2024-07-25 07:25:55 -0700

    end to end csa example script

--- end_to_end_csa_example.sh
+++ end_to_end_csa_example.sh
@@ -3,8 +3,6 @@
 # Below are two examples of how to run the end-to-end scripts:
 # 1. Within-study analysis
 # 2. Cross-study analysis
-# Uncomment and run the one you are you interested in.
-
 
 # # Download the benchmark CSA data
 # wget --cut-dirs=8 -P ./ -nH -np -m https://web.cels.anl.gov/projects/IMPROVE_FTP/candle/public/improve/benchmarks/single_drug_drp/benchmark-data-pilot1/csa_data/
@@ -15,66 +13,63 @@
 # 2. IMPROVE lib
 # TODO finish this
 # -------------------
-current_dir=/lambda_stor/data/apartin/projects/IMPROVE/pan-models/LGBM
+# current_dir=/lambda_stor/data/apartin/projects/IMPROVE/pan-models/GraphDRP
+current_dir=$PWD
 echo "PWD: $current_dir"
-
-# Set env variable for IMPROVE lib
-# export PYTHONPATH=$PYTHONPATH:
-
-# Set env variable for IMPROVE_DATA_DIR (benchmark dataset)
-# export IMPROVE_DATA_DIR="./csa_data/"
-# echo "IMPROVE_DATA_DIR: $IMPROVE_DATA_DIR"
 # ======================================================================
+
+SPLIT=0
 
 # ----------------------------------------
 # 1. Within-study
 # ---------------
 
-# # Preprocess
-# # All preprocess outputs are saved in params["ml_data_outdir"]
-# python lgbm_preprocess_improve.py \
-#     --train_split_file GDSCv1_split_0_train.txt \
-#     --val_split_file GDSCv1_split_0_val.txt \
-#     --test_split_file GDSCv1_split_0_test.txt \
-#     --ml_data_outdir ml_data/GDSCv1-GDSCv1/split_0
+SOURCE=CCLE
+TARGET=CCLE
 
-# # Train
-# # All train outputs are saved in params["model_outdir"]
-# python lgbm_train_improve.py \
-#     --train_ml_data_dir ml_data/GDSCv1-GDSCv1/split_0 \
-#     --val_ml_data_dir ml_data/GDSCv1-GDSCv1/split_0 \
-#     --model_outdir out_model/GDSCv1/split_0
+# Preprocess (improvelib)
+python lgbm_preprocess_improve.py \
+    --train_split_file ${SOURCE}_split_${SPLIT}_train.txt \
+    --val_split_file ${SOURCE}_split_${SPLIT}_val.txt \
+    --test_split_file ${TARGET}_split_${SPLIT}_test.txt \
+    --input_dir ./csa_data/raw_data \
+    --output_dir ./res/${SOURCE}-${TARGET}/split_${SPLIT}
+    # --ml_data_outdir ml_data/${SOURCE}-${TARGET}/split_${SPLIT}
 
-# # Infer
-# # All infer outputs are saved in params["infer_outdir"]
-# python lgbm_infer_improve.py \
-#     --test_ml_data_dir ml_data/GDSCv1-GDSCv1/split_0 \
-#     --model_dir out_model/GDSCv1/split_0 \
-#     --infer_outdir out_infer/GDSCv1-GDSCv1/split_0
+# Train (improvelib)
+python lgbm_train_improve.py \
+    --input_dir ./res/${SOURCE}-${TARGET}/split_${SPLIT} \
+    --output_dir ./res/${SOURCE}-${TARGET}/split_${SPLIT}
+
+# Infer (improvelib)
+python lgbm_infer_improve.py \
+    --input_dir ./res/${SOURCE}-${TARGET}/split_${SPLIT} \
+    --output_dir ./res/${SOURCE}-${TARGET}/split_${SPLIT}
 
 
 # ----------------------------------------
 # 2. Cross-study
 # --------------
 
-# Preprocess
-# All preprocess outputs are saved in params["ml_data_outdir"]
+SOURCE=GDSCv1
+TARGET=CCLE
+
+# Preprocess (improvelib)
 python lgbm_preprocess_improve.py \
-    --train_split_file GDSCv1_split_0_train.txt \
-    --val_split_file GDSCv1_split_0_val.txt \
-    --test_split_file CCLE_all.txt \
-    --ml_data_outdir ml_data/GDSCv1-CCLE/split_0
+    --train_split_file ${SOURCE}_split_${SPLIT}_train.txt \
+    --val_split_file ${SOURCE}_split_${SPLIT}_val.txt \
+    --test_split_file ${TARGET}_all.txt \
+    --input_dir ./csa_data/raw_data \
+    --output_dir ./res/${SOURCE}-${TARGET}/split_${SPLIT}
+    # --ml_data_outdir ml_data/${SOURCE}-${TARGET}/split_${SPLIT}
 
-# Train
-# All train outputs are saved in params["model_outdir"]
+# Train (improvelib)
 python lgbm_train_improve.py \
-    --train_ml_data_dir ml_data/GDSCv1-CCLE/split_0 \
-    --val_ml_data_dir ml_data/GDSCv1-CCLE/split_0 \
-    --model_outdir out_model/GDSCv1/split_0
+    --input_dir ./res/${SOURCE}-${TARGET}/split_${SPLIT} \
+    --output_dir ./res/${SOURCE}-${TARGET}/split_${SPLIT}
 
-# Infer
-# All infer outputs are saved in params["infer_outdir"]
+# Infer (improvelib)
 python lgbm_infer_improve.py \
-    --test_ml_data_dir ml_data/GDSCv1-CCLE/split_0 \
-    --model_dir out_model/GDSCv1/split_0 \
-    --infer_outdir out_infer/GDSCv1-CCLE/split_0
+    --input_dir ./res/${SOURCE}-${TARGET}/split_${SPLIT} \
+    --output_dir ./res/${SOURCE}-${TARGET}/split_${SPLIT}
+
