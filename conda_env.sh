1e51b3b0aacbc0942168210dd9a75d3cc240fa19
Alex Partin <25892264+adpartin@users.noreply.github.com>
2024-08-26 13:10:09 -0700

    update conda_env.sh

--- conda_env.sh
+++ conda_env.sh
@@ -5,21 +5,22 @@ set -e
 # Creating conda env with python 3.7 on Mac M1
 # https://stackoverflow.com/questions/70205633/cannot-install-python-3-7-on-osx-arm64
 
-# conda create -n lgbm_py37 python=3.7 pip --yes
+# CONDA_ENV_NAME=lgbm_py37
+CONDA_ENV_NAME=lgbm_drp_py37
 
-# # Not required
+# Create conda env
+# conda create -n $CONDA_ENV_NAME python=3.7 pip lightgbm=3.1.1 pyarrow=12.0.1 --yes
+conda create -n $CONDA_ENV_NAME python=3.7 pip lightgbm=3.1.1 --yes
+
+# Activate conda env
+conda activate $CONDA_ENV_NAME
+
+# Packages
+conda install conda-forge::pyarrow
+
+# Install CANDLE
+pip install git+https://github.com/ECP-CANDLE/candle_lib@develop
+
+# # Other 
 # conda install -c conda-forge ipdb=0.13.9 --yes
 # conda install -c conda-forge python-lsp-server=1.2.4 --yes
-
-# IMPROVE
-#pip install git+https://github.com/ECP-CANDLE/candle_lib@develop # CANDLE
-
-# Other
-#conda install -c conda-forge lightgbm=3.1.1 --yes # LigthGBM
-#pip install pyarrow=12.0.1 # saves and loads parquet files
-
-conda create -n lgbm_py37 python=3.7 pip lightgbm=3.1.1 --yes
-conda activate lgbm_py37
-pip install pyarrow==12.0.1
-# IMPROVE
-pip install git+https://github.com/ECP-CANDLE/candle_lib@develop # CANDLE
